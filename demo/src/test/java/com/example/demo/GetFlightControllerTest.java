2523a1f54685527d5471d2226e5ccc48885df305
SamZRetzlaff <73126723+SamZRetzlaff@users.noreply.github.com>
2020-12-09 14:45:23 -0800

    all tests working

--- demo/src/test/java/com/example/demo/GetFlightControllerTest.java
+++ demo/src/test/java/com/example/demo/GetFlightControllerTest.java
@@ -29,7 +29,21 @@ public class GetFlightControllerTest {
                 .andExpect(jsonPath("$.ticket.passenger.firstName", is ("Some name")))
                 .andExpect(jsonPath("$.ticket.passenger.lastName", is ("Some other name")));
     }
-    
+    @Test
+    public void testFlight1and2() throws Exception{
+        RequestBuilder request = MockMvcRequestBuilders.get("/flights");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].departs", is ("2017-04-21 14:34")))
+                .andExpect(jsonPath("$[0].ticket.price", is ("200")))
+                .andExpect(jsonPath("$[0].ticket.passenger.firstName", is ("Some name")))
+                .andExpect(jsonPath("$[0].ticket.passenger.lastName", is ("Some other name")))
+                .andExpect(jsonPath("$[1].departs", is ("2017-08-25 01:00")))
+                .andExpect(jsonPath("$[1].ticket.price", is ("300")))
+                .andExpect(jsonPath("$[1].ticket.passenger.firstName", is ("Some name2")))
+                .andExpect(jsonPath("$[1].ticket.passenger.lastName", is ("Some other name2")));
+    }
+
 
 }
 
